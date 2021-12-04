fd33b6507717daedbf271fd22d26803536bf7e78
Daniel Idowu <74189249+dharniel45@users.noreply.github.com>
2021-12-04 19:46:25 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Header from"./components/Header";
 import Home from"./components/Home";
 import './App.css';
-import useMediaQuery from "./useMediaQuery";
 import classNames from 'classnames';
 import styles from "./App.css";
 import Experience from "./components/Experience";
